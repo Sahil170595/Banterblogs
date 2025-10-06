b9480ff4bc84f2399ef7a8de7a54e57716c45d5b
Sahil170595 <147995121+Sahil170595@users.noreply.github.com>
2025-10-05 23:30:10 -0400

    Remove .history files from tracking and update .gitignore

--- .history/banterblogs-nextjs/src/app/chimera/layout_20251005162119.tsx
+++
@@ -1,9 +0,0 @@
-export default function ChimeraLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en" data-theme="chimera">
-      <body className="theme-transition">{children}</body>
-    </html>
-  );
-}
-
-
